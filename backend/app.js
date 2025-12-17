1c54fea3772714f1381a29b6bf9d24f85b693115
SivaKumar202 <130622478+SivaKumar202@users.noreply.github.com>
2025-12-16 23:35:46 -0500

    Backend error handling

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 const app = express();
 import product from "./routes/productRoutes.js";
+import errorHandleMiddleware from "./middleware/error.js"
 
+//Middleware
 app.use(express.json())
+
+//Routes
 app.use("/api/v1", product);
+
+app.use(errorHandleMiddleware)
 
 export default app;
