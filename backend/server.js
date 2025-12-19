ce037968e5781b6c52abc4d17d6f2def08e6d7c9
SivaKumar202 <130622478+SivaKumar202@users.noreply.github.com>
2025-12-19 10:40:09 -0500

    Handle Promise Rejection Errors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,14 @@ import connectionDB from "./config/db.js";
 dotenv.config({ path: "backend/config/config.env" });
 connectionDB();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server is running http://localhost:${port}`);
 });
+
+process.on('unhandledRejection', (err)=>{
+  console.log(`Error : ${err.message}`)
+  console.log(`server is shutting down, due to unhandled promise rejection`)
+  server.close(()=>{
+    process.exit(1)
+  })
+})
