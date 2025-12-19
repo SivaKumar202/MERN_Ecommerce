8da28c1aa9895f3dc19836a73eeecb3d7b0df085
SivaKumar202 <130622478+SivaKumar202@users.noreply.github.com>
2025-12-19 10:25:55 -0500

    Handling Validation Errors

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -1,26 +1,26 @@
 import Products from "../models/productModel.js";
 import HandleError from "../utils/handleError.js";
-
+import handleAsyncError from "../middleware/handleAsyncError.js";
 //1️⃣Creating Products
-export const createProducts = async (req, res) => {
+export const createProducts = handleAsyncError(async (req, res, next) => {
   const product = await Products.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 //2️⃣Get All Products
-export const getAllProducts = async (req, res) => {
+export const getAllProducts = handleAsyncError(async (req, res,next) => {
   const products = await Products.find();
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
 //3️⃣Update Product
-export const updateProduct = async (req, res, next) => {
+export const updateProduct = handleAsyncError(async (req, res, next) => {
   const product = await Products.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -32,11 +32,11 @@ export const updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 //4️⃣Delete Product
 
-export const DeleteProduct = async (req, res, next) => {
+export const DeleteProduct = handleAsyncError(async (req, res, next) => {
   const product = await Products.findByIdAndDelete(req.params.id);
   if (!product) {
     return next(new HandleError("Product Not Found",404))
@@ -45,10 +45,10 @@ export const DeleteProduct = async (req, res, next) => {
     success: true,
     message: "Product is deleted successfully",
   });
-};
+});
 
 //5️⃣ Accessing single Product
-export const getSingleProduct = async (req, res, next) => {
+export const getSingleProduct = handleAsyncError(async (req, res, next) => {
   const product = await Products.findById(req.params.id);
   if (!product) {
     return next(new HandleError("Product Not Found",404))
@@ -57,4 +57,4 @@ export const getSingleProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
